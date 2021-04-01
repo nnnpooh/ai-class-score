dd9fe4fe7147ac0d1399f05dc2442009ff96d9c2
nnnpooh <nnnpooh@gmail.com>
2021-04-02 01:26:01 +0700

    Init

---
+++ src/Data/col.js
@@ -0,0 +1,71 @@
+const midterm = [
+  {
+    fieldname: "M1",
+    colname: "Part 1",
+  },
+  {
+    fieldname: "M2.1",
+    colname: "Problem 2.1",
+  },
+  {
+    fieldname: "M2.2",
+    colname: "Problem 2.2",
+  },
+  {
+    fieldname: "M2.3",
+    colname: "Problem 2.3",
+  },
+  {
+    fieldname: "M2.4",
+    colname: "Problem 2.4",
+  },
+  {
+    fieldname: "M2.5",
+    colname: "Problem 2.5",
+  },
+  {
+    fieldname: "M2.6",
+    colname: "Problem 2.6",
+  },
+  {
+    fieldname: "M3",
+    colname: "Problem 3",
+  },
+
+  {
+    fieldname: "MTotal",
+    colname: "Total",
+  },
+];
+
+const final = [
+  {
+    fieldname: "F1",
+    colname: "Part 1",
+  },
+  {
+    fieldname: "F2",
+    colname: "Problem 2",
+  },
+
+  {
+    fieldname: "FTotal",
+    colname: "Total",
+  },
+];
+
+const overall = [
+  {
+    fieldname: "Assignment",
+    colname: "Assignment",
+  },
+  {
+    fieldname: "MTotal",
+    colname: "Midterm",
+  },
+  {
+    fieldname: "FTotal",
+    colname: "Final",
+  },
+];
+export { midterm, final, overall };
